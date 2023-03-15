dcf9110cd872dfc55b9b50ed4122101794fc365e
eiyen <937915771@qq.com>
2023-03-16 00:24:55 +0800

    add hardhat verify

--- W1-2/hardhat.config.js
+++ W1-2/hardhat.config.js
@@ -1,15 +1,24 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 require('dotenv').config();
 
-const { API_KEY, PRIVATE_KEY } = process.env;
+// const { ProxyAgent, setGlobalDispatcher } = require("undici");
+
+// const proxyAgent = new ProxyAgent("http://127.0.0.1:1080")
+// setGlobalDispatcher(proxyAgent)
+
+const { INFURA_API_KEY, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 
 module.exports = {
   solidity: "0.8.4",
   networks: {
     goerli: {
-      url: `https://goerli.infura.io/v3/${API_KEY}`,
+      url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
       accounts: [PRIVATE_KEY],
       chainId: 5
     }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
   }
 };
