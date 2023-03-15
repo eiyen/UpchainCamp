cf4a4109124eec0aa1ad5f1c88a930d5447bdf9c
eiyen <937915771@qq.com>
2023-03-15 23:08:52 +0800

    test count() function with the calling of owner and addr1

---
+++ W1-2/test/Counter.test.js
@@ -0,0 +1,33 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Counter", function () {
+    let Counter;
+    let counter;
+    let owner;
+    let addr1;
+
+    this.beforeEach(async function () {
+        [owner, addr1] = await ethers.getSigners();
+        Counter = await ethers.getContractFactory("Counter");
+        counter = await Counter.deploy();
+        await counter.deployed();
+    })
+
+    it("Should count when called by the owner", async function () {
+        const initialCounter = await counter.getCounter();
+        expect(initialCounter).to.equal(0);
+
+        const valueToAdd = 5;
+
+        await (await counter.connect(owner).count(valueToAdd)).wait();
+
+        const updatedCounter = await counter.getCounter();
+        expect(updatedCounter).to.equal(initialCounter.add(valueToAdd));
+    })
+
+    it("Should fail when count() is called by someone other than the owner", async function () {
+        const valueToAdd = 5;
+        await expect(counter.connect(addr1).count(valueToAdd)).to.be.revertedWith("Only the contract owner can call this function.");
+    });    
+})
