3d7674b96aa7cb8a67eb16a540aa875faefba09b
eiyen <937915771@qq.com>
2023-03-15 21:37:38 +0800

    add onlyOwner modifier and getCounter function

--- W1-2/contracts/Counter.sol
+++ W1-2/contracts/Counter.sol
@@ -3,9 +3,25 @@
 pragma solidity ^0.8.0;
 
 contract Counter {
-    uint256 public counter;
+    uint256 private counter;
+    address private owner;
 
-    function addCerternNumberToCounter(uint256 _addedNumber) public {
-        counter += _addedNumber;
+    constructor() {
+        counter = 0;
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the contract owner can call this function.");
+        _;
+    }
+
+    function count(uint256 _value) public onlyOwner returns(uint256) {
+        counter += _value;
+        return counter;
+    }
+
+    function getCounter() public view returns(uint256) {
+        return counter;
     }
 }
