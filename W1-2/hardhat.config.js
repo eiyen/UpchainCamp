cf589a8f700c5f544e41bc721d1d8f3296598f8e
eiyen <937915771@qq.com>
2023-03-15 17:20:22 +0800

    add goerli network

--- W1-2/hardhat.config.js
+++ W1-2/hardhat.config.js
@@ -1,6 +1,14 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const { API_KEY, PRIVATE_KEY } = process.env;
+
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: `https://goerli.infura.io/v3/${API_KEY}`,
+      accounts: [PRIVATE_KEY],
+      chainId: 5
+    }
+  }
 };
