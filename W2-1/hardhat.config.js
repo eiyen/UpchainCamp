c126d47ced184ca26df1815bf257595ee16e3da4
eiyen <937915771@qq.com>
2023-03-16 19:05:45 +0800

    deploy to and interact with localhost

--- W2-1/hardhat.config.js
+++ W2-1/hardhat.config.js
@@ -1,3 +1,20 @@
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+
+const { INFURA_RPC_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
+
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: INFURA_RPC_URL,
+      accounts: [PRIVATE_KEY]
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  },
 };
