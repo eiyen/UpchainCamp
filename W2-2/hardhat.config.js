7c713b2a1e965d8026a73c87a733bec85fcb8014
eiyen <937915771@qq.com>
2023-03-30 13:39:17 +0800

    successfully verify contracts

--- W2-2/hardhat.config.js
+++ W2-2/hardhat.config.js
@@ -1,7 +1,12 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
 
-const { INFURA_RPC_URL, PRIVATE_KEY } = process.env;
+const { ProxyAgent, setGlobalDispatcher } = require("undici");
+const proxyAgent = new ProxyAgent("http://127.0.0.1:7890");
+setGlobalDispatcher(proxyAgent);
+
+const { INFURA_RPC_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 
 module.exports = {
   solidity: "0.8.4",
@@ -12,6 +17,10 @@ module.exports = {
     goerli: {
       url: INFURA_RPC_URL,
       accounts: [PRIVATE_KEY],
-    }
-  }
+      chainId: 5,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
